5814001edebbbb2a4f0f1357eb30355d4d012cd6
Denis <116588516+dreamcoiI@users.noreply.github.com>
2024-08-20 20:37:59 +0300

    REFAC_ALEXEY_REFAC

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,14 +7,14 @@ import (
 
 type Config struct {
 	Service  Service
-	Postgres ReadEnvBD
+	Postgres Postgres
 }
 
 type Service struct {
 	Port string `env:"OPTIONHUB_SERVICE_PORT"`
 }
 
-type ReadEnvBD struct {
+type Postgres struct {
 	User     string `env:"OPTIONHUB_SERVICE_POSTGRES_USER"`
 	Password string `env:"OPTIONHUB_SERVICE_POSTGRES_PASSWORD"`
 	Database string `env:"OPTIONHUB_SERVICE_POSTGRES_DB"`
--- scripts/migrations/entrypoint_down_one.sh
+++ scripts/migrations/entrypoint_down_one.sh
@@ -3,4 +3,4 @@
 # Применим миграции
 # ВАЖНО! Для локального запуска вызвать из корня проекта, предварительно подгрузив переменные окружения в терминал командой  `set -a; source <путь к .env файлу>; set +a`
 # Установка goose: go install github.com/pressly/goose/v3/cmd/goose@latest
-goose -dir ./migrations postgres "user=$USER_SERVICE_POSTGRES_USER password=$USER_SERVICE_POSTGRES_PASSWORD dbname=$USER_SERVICE_POSTGRES_DB host=$USER_SERVICE_POSTGRES_HOST port=$USER_SERVICE_POSTGRES_PORT sslmode=disable" down
+goose -dir ./migrations postgres "user=$OPTIONHUB_POSTGRES_USER password=$OPTIONHUB_POSTGRES_PASSWORD dbname=$OPTIONHUB_POSTGRES_DB host=$OPTIONHUB_POSTGRES_HOST port=$OPTIONHUB_POSTGRES_PORT sslmode=disable" down
