100cc7c7ccb5fd6ef9ab9c1656681c473e52bc22
mikhail-kalugin <Alekseevich.m.k@gmail.com>
2025-03-28 21:25:11 +0300

    remove all categories except os

--- internal/service/contract.go
+++ internal/service/contract.go
@@ -12,34 +12,4 @@ type DBRepo interface {
 	GetOsBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
 	GetOsPreview(ctx context.Context) (model.CategoryItemList, error)
 	GetAllOs() (model.CategoryItemList, error)
-
-	GetWorkPlaceBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
-	GetWorkPlacePreview(ctx context.Context) (model.CategoryItemList, error)
-	GetWorkPlaceByID(ctx context.Context, id int64) (string, error)
-	AddWorkPlace(ctx context.Context, name, uuid string) (int64, error)
-
-	GetStudyPlaceBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
-	GetStudyPlacePreview(ctx context.Context) (model.CategoryItemList, error)
-	GetStudyPlaceByID(ctx context.Context, id int64) (string, error)
-	AddStudyPlace(ctx context.Context, name, uuid string) (int64, error)
-
-	GetHobbyBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
-	GetHobbyPreview(ctx context.Context) (model.CategoryItemList, error)
-	GetHobbyByID(ctx context.Context, id int64) (string, error)
-	AddHobby(ctx context.Context, name, uuid string) (int64, error)
-
-	GetSkillBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
-	GetSkillPreview(ctx context.Context) (model.CategoryItemList, error)
-	GetSkillByID(ctx context.Context, id int64) (string, error)
-	AddSkill(ctx context.Context, name, uuid string) (int64, error)
-
-	GetCityBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
-	GetCityPreview(ctx context.Context) (model.CategoryItemList, error)
-	GetCityByID(ctx context.Context, id int64) (string, error)
-	AddCity(ctx context.Context, name, uuid string) (int64, error)
-
-	GetSocietyDirectionBySearchName(ctx context.Context, name string) (model.CategoryItemList, error)
-	GetSocietyDirectionPreview(ctx context.Context) (model.CategoryItemList, error)
-	GetSocietyDirectionByID(ctx context.Context, id int64) (string, error)
-	AddSocietyDirection(ctx context.Context, name, uuid string) (int64, error)
 }
