c4e33e4fccbbd0ea867375bc541c6f68dde09d27
Tony <akatkov89@gmail.com>
2025-05-05 14:39:55 +0300

    fix: added error check

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -37,7 +37,12 @@ func main() {
 	kafkaConfig := kafka_lib.DefaultProducerConfig(cfg.Kafka.Host, cfg.Kafka.Port, cfg.Kafka.SetAttributeTopic)
 
 	producerSetAttribute := kafka_lib.NewProducer(kafkaConfig)
-	defer producerSetAttribute.Close()
+	defer func(producerSetAttribute *kafka_lib.KafkaProducer) {
+		err := producerSetAttribute.Close()
+		if err != nil {
+			logger.Error(fmt.Sprintf("failed to close producer: %v", err))
+		}
+	}(producerSetAttribute)
 
 	optionhubService := service.NewService(dbRepo)
 	optionhubServicev1 := servicev1.NewService(dbRepo, producerSetAttribute)
