5decd6e3940c4d69f3539af1e1804dc7b6784701
asepopov <asepopov@avito.ru>
2025-08-22 20:15:49 +0300

    OPT-5 100% test cover

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -56,5 +58,104 @@ func TestService_GetAttributesMetadata(t *testing.T) {
 		assert.Equal(t, len(mockEntityAttributesIds), len(out.AttributesMetadata))
 	})
 
-	t.Run("fail get attributes metadata", func(t *testing.T) {})
+	t.Run("fail no attributes in", func(t *testing.T) {
+		ctx := context.Background()
+
+		s := NewService(nil)
+
+		_, err := s.GetAttributesMetadata(ctx, &optionhub.GetAttributesMetadataIn{})
+		assert.Error(t, err)
+	})
+
+	t.Run("fail to get entity attributes no rows", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		ctx := context.Background()
+
+		mockEntityAttributesIds := []int64{1, 2}
+
+		mockDbRepo := NewMockDBRepo(ctrl)
+		mockDbRepo.EXPECT().GetEntityAttributesByIds(gomock.Any(), mockEntityAttributesIds).Return(nil, sql.ErrNoRows)
+
+		s := NewService(mockDbRepo)
+		_, err := s.GetAttributesMetadata(ctx, &optionhub.GetAttributesMetadataIn{
+			EntityAttributeIds: mockEntityAttributesIds,
+		})
+		assert.Error(t, err)
+	})
+
+	t.Run("fail to get entity attributes err", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		ctx := context.Background()
+
+		mockEntityAttributesIds := []int64{1, 2}
+
+		mockDbRepo := NewMockDBRepo(ctrl)
+		mockDbRepo.EXPECT().GetEntityAttributesByIds(gomock.Any(), mockEntityAttributesIds).Return(nil, errors.New("some error"))
+
+		s := NewService(mockDbRepo)
+		_, err := s.GetAttributesMetadata(ctx, &optionhub.GetAttributesMetadataIn{
+			EntityAttributeIds: mockEntityAttributesIds,
+		})
+		assert.Error(t, err)
+	})
+
+	t.Run("fail to get attributes no rows", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		ctx := context.Background()
+
+		mockEntityAttributesIds := []int64{1, 2}
+		mockEntityAttributesMeta := []model.EntityAttribute{
+			{
+				EntityAttributeID: 1,
+				EntityType:        "USER",
+				AttributeID:       123,
+			},
+			{
+				EntityAttributeID: 2,
+				EntityType:        "USER",
+				AttributeID:       321,
+			},
+		}
+		mockAttributesIds := []int64{123, 321}
+
+		mockDbRepo := NewMockDBRepo(ctrl)
+		mockDbRepo.EXPECT().GetEntityAttributesByIds(gomock.Any(), mockEntityAttributesIds).Return(mockEntityAttributesMeta, nil)
+		mockDbRepo.EXPECT().GetAttributesByIds(gomock.Any(), mockAttributesIds).Return(nil, sql.ErrNoRows)
+
+		s := NewService(mockDbRepo)
+		_, err := s.GetAttributesMetadata(ctx, &optionhub.GetAttributesMetadataIn{
+			EntityAttributeIds: mockEntityAttributesIds,
+		})
+		assert.Error(t, err)
+	})
+
+	t.Run("fail to get attributes err", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		ctx := context.Background()
+
+		mockEntityAttributesIds := []int64{1, 2}
+		mockEntityAttributesMeta := []model.EntityAttribute{
+			{
+				EntityAttributeID: 1,
+				EntityType:        "USER",
+				AttributeID:       123,
+			},
+			{
+				EntityAttributeID: 2,
+				EntityType:        "USER",
+				AttributeID:       321,
+			},
+		}
+		mockAttributesIds := []int64{123, 321}
+
+		mockDbRepo := NewMockDBRepo(ctrl)
+		mockDbRepo.EXPECT().GetEntityAttributesByIds(gomock.Any(), mockEntityAttributesIds).Return(mockEntityAttributesMeta, nil)
+		mockDbRepo.EXPECT().GetAttributesByIds(gomock.Any(), mockAttributesIds).Return(nil, errors.New("some error"))
+
+		s := NewService(mockDbRepo)
+		_, err := s.GetAttributesMetadata(ctx, &optionhub.GetAttributesMetadataIn{
+			EntityAttributeIds: mockEntityAttributesIds,
+		})
+		assert.Error(t, err)
+	})
 }
