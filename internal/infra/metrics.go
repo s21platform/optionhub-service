c2b46dcc301a207bdf0e07a181bdec5145d0f6a8
asepopov <asepopov@avito.ru>
2025-08-22 20:30:47 +0300

    OPT-5 fix metrics

--- internal/infra/metrics.go
+++ internal/infra/metrics.go
@@ -21,7 +21,7 @@ func MetricsInterceptor(metrics *pkg.Metrics) func(ctx context.Context, req inte
 		handler grpc.UnaryHandler) (
 		interface{}, error) {
 		t := time.Now()
-		method := strings.Trim(strings.ReplaceAll(info.FullMethod, "/", "_"), "_")
+		method := strings.Join(strings.Split(info.FullMethod, "/")[1:], "")
 		metrics.Increment(method)
 
 		ctx = context.WithValue(ctx, config.KeyMetrics, metrics)
